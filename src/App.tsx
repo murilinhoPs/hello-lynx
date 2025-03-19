c4f83742f759633afead7b0721bdb68fdd5c722b
murilinhops <murilinhorps@gmail.com>
2025-03-19 10:41:08 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './index.css';
+
+export function App() {
+  return (
+    <view style={{ marginTop: '20%' }}>
+      <text
+        style={{
+          fontSize: '32px',
+          fontWeight: 'bold',
+          alignSelf: 'center',
+          color: '#fff',
+        }}
+      >
+        Hello World!
+      </text>
+    </view>
+  );
+}
